e76d0f086a1b4413dd200e2141cea044da05fcea
zavodtsvetov <mitfahov@gmail.com>
2025-02-06 20:35:31 +0500

    merge one more time

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 console.log('Hello World')
 
-<<<<<<< HEAD
 const userAge = 35
 
 if (userAge >= 18) {
     console.log('Вам есть 18')
 } else {
     console.log('Ну нет вам еще 18-ти')
-=======
-const age = 20
-if (age >= 18) {
-    console.log('Вы совершеннолетний')
-} else {
-    console.log('Вы не достигли 18 лет!')
->>>>>>> feat/user-access
 }
